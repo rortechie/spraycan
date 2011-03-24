0b5e39a63e200076f1055aa4549b90e34e982603
Brian Quinn <briandquinn@gmail.com>
2011-03-24 12:08:34 +0000

    Improved tooltip display logic

--- public/javascripts/hooks.deface.js
+++ public/javascripts/hooks.deface.js
@@ -3,56 +3,13 @@ var show_frames = true;
 var frame_level = 1;
 var current_hooks = new Array;
 
-function resize_hook_frames(){
+function show_hook_frames(){
   $jQ.each($jQ("[data-layer='" + frame_level + "']"), function(i, hook){
     var hook = $jQ(hook);
     var layer = hook.attr('data-layer');
 
-    if(frame_level==layer){
-      hook.addClass('deface_hook_frame');
-
-      if(hook.data('qtip')==undefined){
-        hook_name = hook.attr('data-hook')
-        hook.qtip({
-          content: '<h4>' + hook_name+'</h4><a href="/deface#view_overrides/edit/' + hook_name + '" target="_top">Edit</a> | <a href="/deface#view_overrides/new/' + hook_name + '" target="_top">New</a>',
-          show: { solo: true },
-          hide: { when: { event: 'unfocus' }, delay: 2000 },
-          position: {
-            adjust: { screen: true },
-            corner: { tooltip: 'bottomMiddle', target: 'topMiddle' }
-          },
-          style: { border: { width: 4, radius: 6 },
-                   padding: 6,
-                   textAlign: 'center',
-                   tip: true,
-                   name: 'dark',
-                   classes: { tooltip: 'deface-qtip' }
-          },
-          api: {
-            beforeShow: function(){
-              if(frame_level +'' == $(this.elements.target).attr('data-layer')){
-                return true
-              }else{
-                return false
-              }
-            },
-
-            onRender:function() {
-              //handlers for bubbles
-            }
-          }
-
-        });
-      }else{
-        hook.qtip("enable");
-      }
-
-      current_hooks[current_hooks.length] = hook;
-    }
-
+    hook.addClass('deface_hook_frame');
   });
-
-
 }
 
 function hook_zoom(in_or_out){
@@ -80,20 +37,63 @@ function hook_zoom(in_or_out){
       $jQ('.deface_hook_frame').removeClass('deface_hook_frame');
 
       if(show_frames){
-        resize_hook_frames();
+        show_hook_frames();
       }
     }
   }
 }
 
+function create_tip(e){
+  var hook = $jQ(e.currentTarget);
+  var layer = hook.attr('data-layer');
+
+  if(frame_level==layer){
+
+    if(hook.data('qtip')==undefined){
+      hook_name = hook.attr('data-hook');
+
+      hook.qtip({
+        content: '<h4>' + hook_name+'</h4><a href="/deface#view_overrides/edit/' + hook_name + '" target="_top">Edit</a> | <a href="/deface#view_overrides/new/' + hook_name + '" target="_top">New</a>',
+        show: { ready: true, solo: true, delay: 0, effect: { type: 'none', length: 0 } },
+        hide: { fixed: true, when: { event: 'unfocus' }, delay: 2000, effect: { type: 'none', length: 0 } },
+        position: {
+          adjust: { screen: true },
+          corner: { tooltip: 'bottomMiddle', target: 'topMiddle' }
+        },
+        style: { border: { width: 4, radius: 6 },
+                 padding: 6,
+                 textAlign: 'center',
+                 tip: true,
+                 name: 'dark',
+                 classes: { tooltip: 'deface-qtip' }
+        }
+
+      });
+    }else{
+       hook.qtip("enable");
+    }
+
+    hook.qtip("show");
+    hook.qtip("focus");
+  }
+
+}
+
+function kill_tip(e){
+  var hook = $jQ(e.currentTarget);
+
+  if(hook.data('qtip')!=undefined){
+     //hook.qtip("hide");
+     //hook.qtip("disable");
+  }
+}
+
 $jQ(function() {
   if(top.App!=undefined){
-    //set initial placement for hook frames
-    resize_hook_frames();
+    show_hook_frames();
 
-    $jQ(window).resize(function() {
-      resize_hook_frames();
-    });
+    $('[data-hook]').mousemove(create_tip).mouseleave(kill_tip);
+
   }
 });
 
