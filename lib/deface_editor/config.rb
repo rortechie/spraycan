f3b711947fecb443369143dbf1318fb2e1cbc682
Brian Quinn <briandquinn@gmail.com>
2011-04-04 19:12:56 +0100

    Make configuration less Spree specific.

--- lib/deface_editor/config.rb
+++ lib/deface_editor/config.rb
@@ -1,13 +1,17 @@
 module DefaceEditor
   module Config
-
+    #allows overrides to be applied to views
     @@enable_overrides = true
     mattr_accessor :enable_overrides
 
+
+    #allows editor to be used
     @@enable_editor = true
     mattr_accessor :enable_editor
 
-    @@editor_virtual_paths = ["shared/_head", "layouts/spree_application", "layouts/admin"]
+    #which virutal paths (templates) should the site specific
+    #deface helpers be included in (hint, should contain HEAD tag)
+    @@editor_virtual_paths = ["layouts/application"]
     mattr_accessor :editor_virtual_paths
 
   end
